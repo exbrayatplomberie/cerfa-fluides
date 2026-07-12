e287916df8d3b6ede982b6177b6480d69085d4b9
exbrayatplomberie <ent.exbrayat@gmail.com>
2026-07-12 13:45:42 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,8 @@
-const CACHE='cerfa-fluides-exbrayat-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='cerfa-exbrayat-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png',
+  './attestation-capacite-exbrayat.pdf',
+  './cerfa_15497-04.pdf',
+  './pdf-lib.min.js'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>event.request.mode==='navigate'?caches.match('./index.html'):undefined)))});
