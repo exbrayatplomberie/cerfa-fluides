09cb3600c1d960b12e6389514c8b9e2369bbe6a0
exbrayatplomberie <ent.exbrayat@gmail.com>
2026-07-17 17:56:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
-const CACHE='exbrayat-pro-v0.4.2.8-numero-date-heure';
-const ASSETS=['./','./index.html','./style.css?v=0.4.2.8','./app.js?v=0.4.2.8','./pdf-lib.min.js?v=0.4.2.8','./manifest.webmanifest','./icon.svg','./cerfa_15497-04.pdf','./attestation-capacite.pdf','./logo-exbrayat.png'];
+const CACHE='exbrayat-pro-v0.4.2.9-pense-bete';
+const ASSETS=['./','./index.html','./style.css?v=0.4.2.9','./app.js?v=0.4.2.9','./pdf-lib.min.js?v=0.4.2.9','./manifest.webmanifest','./icon.svg','./cerfa_15497-04.pdf','./attestation-capacite.pdf','./logo-exbrayat.png','./pense-bete-mesures-clim-reversible.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
